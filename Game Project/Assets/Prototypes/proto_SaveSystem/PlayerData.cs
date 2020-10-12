1c582dcc0a8e5e7f6eeac387c005f95b8ddb7bcf
cameron-kent-sae <1020666@student.sae.edu.au>
2020-10-12 16:14:53 +1100

    Basic Save System
    Implemented a basic save/load system that will take variables from a player object and create a 'save file' in binary that can be loaded in.
    can be modified to suit needs of game when required

---
+++ Game Project/Assets/Prototypes/proto_SaveSystem/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    public int lives;
+    public float playTime;
+    public int progress;
+
+    public PlayerData(Player player)
+    {
+        lives = player.lives;
+        playTime = player.playTime;
+        progress = player.progress;
+    }
+}
