adc214238d4a389af3339521a943fb33d5e4ba35
keith1755 <62937410+keith1755@users.noreply.github.com>
2020-10-17 17:41:31 +1100

    Trap prototype (Shooter)
    Shooter trap
    -Trigger type
    -Frequency type
    c#: Tshooter, Tbullet
    
    Location: ->Scenes --> Traps

---
+++ Game Project/Assets/Scenes/Traps/Shooter Trap/Tbullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tbullet : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public Vector2 dir = new Vector2(0, 0);
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        //object move, base on variable  
+        rb.velocity = dir;
+    }
+
+
+    // kill player on touched 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("kill");
+            Destroy(other.gameObject);
+
+        }
+    }
+
+
+}
---
+++ Game Project/Assets/Scenes/Traps/Shooter Trap/Tshooter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tshooter : MonoBehaviour
+{
+    public GameObject shootTrap;  //Trap Object
+    public Tbullet bulletPrefab; //bullet Object
+    public float bulletSpeed; //bullet speed
+    private float time = 0;
+
+    [Range(1, 5)]
+    public float bulletDelay; //Delay time
+
+    public bool fromRight = false; //filp the trap's direction
+    public bool shoot; //trigger trap or keep shooting
+
+    private Vector2 dir2; //bullet shoot direction and force
+
+    void Start()
+    {
+        //using to filp the trap and the bullet direction
+        if (fromRight == true)
+        {
+            Vector3 newScale = shootTrap.transform.localScale;
+            newScale.x *= -1; //filp the object
+            shootTrap.transform.localScale = newScale;
+
+            dir2 = new Vector2(bulletSpeed, 0); //bullet shoot direction and speed
+            dir2.x *= -1; //filp the object
+        }
+        else
+        {
+            dir2 = new Vector2(bulletSpeed, 0); //bullet shoot direction and speed
+        }
+
+    }
+
+    void Update()
+    {
+        //Instantiaste the bullet on the arrowPrefab location and shoot, can adjust the delay in the inspector
+        if (shoot == true)
+        {
+
+            if (time < Time.time)
+            {
+                Tbullet bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as Tbullet;
+                bullet.dir = dir2;
+
+                time = Time.time + bulletDelay; //bullet delay time
+            }
+        }
+    }
+
+    //Trigger the trap by the player when enter the collision , exit to switch off
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            shoot = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            shoot = false;
+        }
+    }
+}
