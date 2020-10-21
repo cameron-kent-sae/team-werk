0f89b8d76e18979750fccac928d4ae454a09ed16
TheBard523 <72112073+TheBard523@users.noreply.github.com>
2020-10-21 15:12:49 +1100

    Troubleshooting Grappling
    Troubleshooting grapple mechanic for the laser whip

---
+++ Game Project/Assets/Scripts/grappler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grappler : MonoBehaviour
+{
+    public Transform whipTarget;
+    public LineRenderer whipRenderer;
+    public DistanceJoint2D distanceJoint;
+    public Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceJoint.enabled = false;
+        whipRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Grapple"))
+        {
+            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            whipRenderer.SetPosition(0, mousePos);
+            whipRenderer.SetPosition(1, transform.position);
+            distanceJoint.connectedAnchor = mousePos;
+            distanceJoint.enabled = true;
+            whipRenderer.enabled = true;
+        }
+        else if (Input.GetButtonUp("Grapple"))
+        {
+            distanceJoint.enabled = false;
+            whipRenderer.enabled = false;
+        }
+        if (distanceJoint.enabled)
+        {
+            whipRenderer.SetPosition(1, transform.position);
+        }
+    }
+}
