d6b0bc897a92543202a0d67cf82105be34ff2cab
cameron-kent-sae <1020666@student.sae.edu.au>
2020-10-21 12:52:22 +1100

    created menu controllers
    set up menu controllers for main menu, options menu and load menu.
    implemented scene load navigation between these scenes

---
+++ Game Project/Assets/proto gameMenu/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private Button startBtn, optionsBtn, quitBtn;
+
+    private void Start()
+    {
+        //Find the menu buttons and add listeners to click
+        startBtn = GameObject.Find("Button_Start").GetComponent<Button>();
+        optionsBtn = GameObject.Find("Button_Options").GetComponent<Button>();
+        quitBtn = GameObject.Find("Button_Quit").GetComponent<Button>();
+
+        startBtn.onClick.AddListener(StartGame);
+        optionsBtn.onClick.AddListener(StartOptions);
+        quitBtn.onClick.AddListener(QuitGame);
+    }
+
+    //Load the game select menu
+    public void StartGame()
+    {
+        SceneManager.LoadScene("p_LoadMenu");
+    }
+
+    //Load the game options menu
+    public void StartOptions()
+    {
+        SceneManager.LoadScene("p_OptionsMenu");
+    }
+
+    //Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
