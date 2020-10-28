cbeb3faddf05e833681133bc4681c7070ae9caab
keith1755 <62937410+keith1755@users.noreply.github.com>
2020-10-28 15:22:08 +1100

    Level 2 prototype is ready for testing

--- Game Project/Assets/Scenes/Traps/Shooter Trap/Tbullet.cs
+++ Game Project/Assets/Scenes/Traps/Shooter Trap/Tbullet.cs
@@ -19,12 +19,12 @@ public class Tbullet : MonoBehaviour
     // Destroy when hitting the tilemap 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Tilemap")|| other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Damage"))
+        if (other.gameObject.CompareTag("Tilemap")|| other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Wall"))
         {
+            print("test");
             Destroy(gameObject);
 
         }
     }
-
 
 }
