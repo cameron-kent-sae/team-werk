ef9ec347a09954222c018d66d240230c52aac7b2
TheBard523 <72112073+TheBard523@users.noreply.github.com>
2020-10-28 13:14:34 +1100

    Added player controller
    Added current player controller prototype

---
+++ Game Project/Assets/Prototype Elements/Scripts/enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // public Animator animator;
+    
+    public int maxHealth = 100;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage (int damage)
+    {
+        currentHealth -= damage;
+
+        // Play injury animation
+        //animator.SetTrigger("hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy Died");
+        // Die animation
+        //animator.SetBool("isDead", true);
+
+        // Disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
