4a2b5abfbe45a591b598c9a0c3195009cd11ff64
TheBard523 <72112073+TheBard523@users.noreply.github.com>
2020-11-06 19:24:25 +1100

    Push/Pull Mechanic T/S
    Troubleshooting Push/Pull mechanic

--- Game Project/Assets/pushblock.cs
+++ Game Project/Assets/pushblock.cs
@@ -6,6 +6,7 @@ public class pushblock : MonoBehaviour
 {
     public float distance = 1f;
     public LayerMask movable;
+    private GameObject block;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,34 @@ public class pushblock : MonoBehaviour
     void Update()
     {
         Physics2D.queriesStartInColliders = false;
-        RaycastHit2D hit =  Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, movable);
+        RaycastHit2D hit =  Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, distance, movable);
+
+        /*if (hit.collider != null && hit.collider.gameObject.tag == "pushblock" && Input.GetButtonDown("MoveBlock"))
+        {
+            block = hit.collider.gameObject;
+
+            block.GetComponent<FixedJoint2D>().enabled = true;
+            block.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+        }
+        else if (Input.GetButtonUp("MoveBlock"))
+        {
+            block.GetComponent<FixedJoint2D>().enabled = false;
+        }*/
+        if (hit.collider != null && hit.collider.gameObject.tag == "pushblock" && Input.GetButtonDown("MoveBlock"))
+        {
+            block = hit.collider.gameObject;
+
+            this.GetComponent<FixedJoint2D>().enabled = true;
+            this.GetComponent<FixedJoint2D>().connectedBody = block.GetComponent<Rigidbody2D>();
+        }
+        else if (Input.GetButtonUp("MoveBlock"))
+        {
+            this.GetComponent<FixedJoint2D>().enabled = false;
+        }
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.left * transform.localScale.x * distance);
     }
 }
