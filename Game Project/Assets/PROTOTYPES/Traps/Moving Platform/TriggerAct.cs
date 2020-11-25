e38a5872a68b2eb5f86caf67d44a78798573dcef
keith1755 <62937410+keith1755@users.noreply.github.com>
2020-11-25 13:01:06 +1100

    World 3 update
    +Traps
    angry rock, explode lava, trigger platform

---
+++ Game Project/Assets/PROTOTYPES/Traps/Moving Platform/TriggerAct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAct : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) ;
+        print("ok");
+        GetComponent<Tpathing>().enabled = true;
+    }
+}
