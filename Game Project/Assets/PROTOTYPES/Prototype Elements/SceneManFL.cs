4cef0025927aa94da7a27b8adaf6e43dd1424da1
keith1755 <62937410+keith1755@users.noreply.github.com>
2020-11-27 00:41:14 +1100

    + menu bar for the prototype
    + simple menu bar
    Now, we can export the simple build for Milestone 4 - Feature Lock Submission status

---
+++ Game Project/Assets/PROTOTYPES/Prototype Elements/SceneManFL.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManFL : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Planet 1
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+    //Planet 2
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Level8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void Level9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void Level10()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    //Planet 3
+    public void Level11()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void Level12()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void Level13()
+    {
+        SceneManager.LoadScene(13);
+    }
+    public void Level14()
+    {
+        SceneManager.LoadScene(14);
+    }
+    public void Level15()
+    {
+        SceneManager.LoadScene(15);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
