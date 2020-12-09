93a74b2f6c49be0fee9842e665bf6de212b4dfe4
Chibi-Burrito <70257495+Chibiburr@users.noreply.github.com>
2020-12-09 11:28:58 +1100

    MainMenuControls
    Main Menu button functionalities, sound and transitions.

---
+++ Game Project/Assets/gameMenu/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+
+{
+    public Button playButton;
+    public string gameScene;
+    public void Start()
+    {
+        playButton = GameObject.Find("MB_StartGame").GetComponent<Button>();
+        playButton.onClick.AddListener(PlayGame);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
