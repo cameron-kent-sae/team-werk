ad101b007475461699227d143e5ab46be9a2c454
cameron-kent-sae <1020666@student.sae.edu.au>
2020-12-16 10:30:05 +1100

    Level Transition script
    created a quick scene manager to change between levels

---
+++ Game Project/Assets/Scripts/LevelTransitionsManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitionsManager : MonoBehaviour
+{
+    // declare the next level to load in the editor
+    public string nextLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
